dc671f94c583bccd6526d5961ee54c704e0c41b8
ByeonggakYu <qudrkr0815@korea.ac.kr>
2021-03-24 14:04:57 +0900

    0324

---
+++ ex1181/ex1181.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <string> 
+#include <algorithm> // sort 함수가 있음
+
+using namespace std;
+
+int		N;
+
+bool	tool(string a, string b)
+{
+	if (a.length() == b.length())
+		return (a < b);
+	else
+		return (a.length() < b.length());
+}
+
+int		main(void)
+{
+	cin >> N;
+	string	word;
+	vector<string> words;
+
+	for (int i = 0; i < N; i++)
+	{
+		cin >> word;
+		if (find(words.begin(), words.end(), word) == words.end())
+			words.push_back(word);
+	}
+	sort(words.begin(), words.end(), tool);
+	for (int i = 0; i < words.size(); i++)
+		cout << words[i] << '\n';
+	return (0);
+}
+
+
+	
+
+
+	
