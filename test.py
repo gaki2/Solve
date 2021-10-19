4b762d5b556d682ff1f0dfdbda946b6b2d85725d
ByeonggakYu <qudrkr0815@korea.ac.kr>
2021-10-19 15:23:42 +0900

    [practice]

--- test.py
+++ test.py
@@ -1,12 +1,21 @@
-from collections import deque
+import time
 
-queue = deque([4])
+arr = []
 
+for j in range(10000):
+    arr.append(j)
 
-def test(queue):
-    queue.append(4)
+arr2 = []
 
+for i in range(10000):
+    arr2.append(arr)
 
-test(queue)
+start = time.time()  # 시작 시간 저장
+cnt = 0
+for i in range(10000):
+    for j in range(10000):
+        cnt += arr2[j][i]
 
-print(queue)
+print(cnt)
+
+print("time :", time.time() - start)  # 현재시각 - 시작시간 = 실행 시간
